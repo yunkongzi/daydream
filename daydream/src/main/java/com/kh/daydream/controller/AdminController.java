dfd42d7052601a75d04ed25711a85f47836ed08c
yunkongzi <wkddbswl47@hanmail.net>
2022-01-03 15:38:11 +0900

    admin controller 생성

---
+++ daydream/src/main/java/com/kh/daydream/controller/AdminController.java
@@ -0,0 +1,17 @@
+package com.kh.daydream.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	
+	// 회원 가입 폼
+		@RequestMapping(value="/program_regist", method=RequestMethod.GET)
+		public String memberRegistForm() {
+			return "admin/program_regist";
+		}
+
+}
