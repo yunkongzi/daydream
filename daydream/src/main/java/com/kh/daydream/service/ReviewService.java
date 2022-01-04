fbc40a1949dd983bc99166e77dfd452475477bea
ohsora <sasfoof@naver.com>
2022-01-04 14:27:59 +0900

    review-mapper ReviewService -> 수정

--- daydream/src/main/java/com/kh/daydream/service/ReviewService.java
+++ daydream/src/main/java/com/kh/daydream/service/ReviewService.java
@@ -2,9 +2,12 @@ package com.kh.daydream.service;
 
 import javax.inject.Inject;
 
+import org.springframework.stereotype.Service;
+
 import com.kh.daydream.dao.ReviewDao;
 import com.kh.daydream.vo.ReviewVo;
 
+@Service
 public class ReviewService {
 	
 	@Inject
