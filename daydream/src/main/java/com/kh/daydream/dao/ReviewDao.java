2a7f938a7bf6a6696da8d87d235751287e53cc45
tbjg <tbjg@naver.com>
2022-01-06 15:20:23 +0900

    전부 수정~~

--- daydream/src/main/java/com/kh/daydream/dao/ReviewDao.java
+++ daydream/src/main/java/com/kh/daydream/dao/ReviewDao.java
@@ -9,18 +9,24 @@ import com.kh.daydream.vo.ReviewVo;
 
 @Repository
 public class ReviewDao {
-	private static final String NAMESPACE = "com.kh.mappers.review.";
+	private static final String NAMESPACE = "com.kh.daydream.mappers.review.";
 	
 	@Inject
 	private SqlSession sqlsession;
 	
-	
-	public void getDate() {
-		Object obj = sqlsession.selectOne(NAMESPACE, "getDate");
-		System.out.println(obj);
-	}
-	
+	// 리뷰 추가
 	public void insertReview (ReviewVo reviewVo) {
 		sqlsession.insert(NAMESPACE + "insertReview", reviewVo);
+	}
+	
+	// 리뷰 정보 보기
+	public ReviewVo selectById(int bno) {
+		ReviewVo reviewVo = sqlsession.selectOne(NAMESPACE + "selectById", bno);
+		return reviewVo;
+	}
+	
+	// 리뷰 수정
+	public void updateReview(ReviewVo reviewVo) {
+		sqlsession.update(NAMESPACE + "updateReview", reviewVo);
 	}
 }
