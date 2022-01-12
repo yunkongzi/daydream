3cdbaff89e440df68677367d3b4a3d4adab27b2f
yunkongzi <wkddbswl47@hanmail.net>
2022-01-12 16:15:44 +0900

    program file_upload editing

--- daydream/src/main/java/com/kh/daydream/controller/AdminController.java
+++ daydream/src/main/java/com/kh/daydream/controller/AdminController.java
@@ -20,6 +20,9 @@ import com.kh.daydream.vo.ProgramVo;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	
+	private static final String programVo = null;
+	private static final String UPLOAD_PATH = "//192.168.0.80/programpic/";
 
 	@Inject
 	private ProgramService programService;
@@ -49,11 +52,11 @@ public class AdminController {
 	// 프로그램 등록 처리, 파일 첨부
 		@RequestMapping(value = "/regist_run", method = RequestMethod.POST)
 		public String programRegistRun(MultipartHttpServletRequest request, int[] time_no) throws Exception {
-			MultipartFile multi = request.getFile("file_name");
+			MultipartFile multi = request.getFile("file_image");
 			String filename = multi.getOriginalFilename();
 			String uuid = UUID.randomUUID().toString();
-			String file_name = uuid + "_" + filename;
-			multi.transferTo(new File("\\192.168.0.80/programpic/" + uuid + "_" + file_name));
+			String file_image = uuid + "_" + filename;
+			multi.transferTo(new File(UPLOAD_PATH + uuid + "_" + file_image));
 			
 			String class_name = request.getParameter("class_name");
 			String price = request.getParameter("price");
@@ -61,13 +64,12 @@ public class AdminController {
 			String personnel = request.getParameter("personnel");
 			String class_intro = request.getParameter("class_intro");
 			int class_no = Integer.parseInt(request.getParameter("class_no"));
-			String file_image = request.getParameter("file_image");
 			ProgramVo programVo = new ProgramVo(class_name, price, target, 
 					personnel,class_intro, class_no, time_no, file_image);
 			System.out.println("AdminController, programRegistRun, programVo:" + programVo); 
 			
-			programService.insertProgram(programVo);
-			System.out.println("program_regist:" + programVo);
+//			programService.insertProgram(programVo);
+//			System.out.println("program_regist:" + programVo);
 			return "redirect:/admin/program_list";
 		}
 
