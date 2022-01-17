234b8a48057b84a773620655a93d58014ec14171
DAYOUNGsss <dud236@naver.com>
2022-01-17 17:12:06 +0900

    []

--- daydream/src/main/java/com/kh/daydream/controller/ReservationController.java
+++ daydream/src/main/java/com/kh/daydream/controller/ReservationController.java
@@ -1,5 +1,6 @@
 package com.kh.daydream.controller;
 
+
 import java.util.List;
 
 import javax.inject.Inject;
@@ -11,14 +12,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.kh.daydream.service.ProgramService;
 import com.kh.daydream.service.ReservationService;
-
 import com.kh.daydream.vo.MemberVo;
 import com.kh.daydream.vo.ProgramVo;
 import com.kh.daydream.vo.ReservationListVo;
-
 import com.kh.daydream.vo.ReservationTimeVo;
 import com.kh.daydream.vo.ReservationVo;
+
 
 @Controller
 @RequestMapping("/reservation")
@@ -27,40 +28,49 @@ public class ReservationController {
 	private static final String RESERVED = "1";
 	private static final String INCLASS = "2";
 	private static final String FINISH = "3";
+
 	
 	@Inject
 	private ReservationService reservationService;
+	@Inject
+	private ProgramService programService;
 	
-		// 예약 등록 폼
+		// 예약등록폼
 		@RequestMapping(value="/reservation_regist/{class_no}", method=RequestMethod.GET)
-		public String reservationRegistForm(@PathVariable("class_no") int class_no, Model model) {
-			List<ReservationTimeVo> timeList = reservationService.selectTimeList(class_no);
+		public String reservationRegistForm(@PathVariable("class_no") int class_no, Model model){
+			List<ReservationTimeVo> timeList = reservationService.selectTimeList(class_no);	
 			model.addAttribute("timeList", timeList);
-			System.out.println("ReservationController , reservationRegistForm , timeList >> " + timeList);
+			ProgramVo programVo = programService.selectByClassNo(class_no);
+			model.addAttribute("programVo",programVo);
+			model.addAttribute("class_no", class_no);
+			System.out.println("ReservationController, reservationRegistFrom, class_no : " + class_no);
+			System.out.println("ReservationController , reservationRegistForm , timeList : " + timeList);
 			return "reservation/reservation_regist";
 	}
-		// 예약 등록 처리
+		// 예약등록처리
 		@RequestMapping(value="/regist_run", method=RequestMethod.POST)
-		public String reservationResgistrun(ReservationVo reservationVo) {
-			System.out.println("RerservationController, insertRerservation, insertReservation:"+reservationVo);
+		public String reservationResgistrun(ReservationVo reservationVo, HttpSession session){
+			MemberVo memberVo = (MemberVo)session.getAttribute("memberVo");
+			String user_id = memberVo.getUser_id();
+			reservationVo.setUser_id(user_id);
+			System.out.println("ReservationController, reservationResgistrun, reservationVo : " + reservationVo);
 			reservationService.insertReservation(reservationVo);
-
 			return "redirect:/program_intro";
 	} 
-		//예약 목록
+		//예약현황
 		 @RequestMapping(value="/rev_list", method=RequestMethod.GET)
-		   public String reservationList(Model model,HttpSession session) {
-			 MemberVo memberVo = (MemberVo) session.getAttribute("memberVo");
-
-				if (memberVo == null || !memberVo.getUser_id().equals("kongzi")) {
-					return "redirect:/main";
-				}
-			  List<ReservationListVo> revList =  reservationService.reservationList();
-			  model.addAttribute("revList", revList);
+		   public String reservationList(int class_no, Model model, HttpSession session, ReservationVo reservationVo) {
+			  MemberVo memberVo = (MemberVo)session.getAttribute("memberVo");
+			  String user_id = memberVo.getUser_id();
+			  reservationVo.setUser_id(user_id);
+			  ProgramVo programVo = programService.selectByClassNo(class_no);
+			  model.addAttribute("programVo",programVo);
+			  model.addAttribute("class_no", class_no);
+			  List<ReservationListVo> allList =  reservationService.reservationList();
+			  model.addAttribute("allList", allList);
 			  return "/admin/rev_list";
- }
-
-//		// 예약 수정 폼
+		   }
+//		// 예약 수정
 //		@RequestMapping(value = "/reservation_modify", method = RequestMethod.GET)
 //		public String programModify(int class_no, Model model) {
 //			System.out.println("class_no" + class_no);
@@ -71,23 +81,19 @@ public class ReservationController {
 //			return "/admin/program_modify";
 //		}
 //	
-//		// 예약 수정 처리
+//		// 수정 처리
 //		@RequestMapping(value = "/reservation_run", method = RequestMethod.POST)
 //		public String updateReservation(ReservationVo reservationVo) {
 //			reservationService.updateReservation(reservationVo);
 //			return "redirect:/reservation/reservation_list";
 //		}
 //	
-//		// 예약 취소
+//		// 삭제
 //		@RequestMapping(value = "/deleteReservation", method = RequestMethod.GET)
 //		public String deleteReservation(int class_no) {
 //			reservationService.deleteReservation(class_no);
 //			return "redirect:/reservation/reservation_list";
 //		}
 
-
-
-		
-		
 
 }
